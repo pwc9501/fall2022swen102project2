2d5d04d67358bf38c354a93c1b41ca55cc046758
LiangChu870 <lc2410@g.rit.edu>
2022-10-06 11:22:38 -0400

    preset text has been commited

---
+++ randomText.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class randomText{
+    private File file = new File("preset_text.txt"); 
+    private BufferedReader br = null; 
+    private Random random = new Random();
+
+    private int totallines(){
+        int total = 0;
+        try{
+            br = new BufferedReader(new FileReader(file));
+            while((br.readLine()) != null){
+                total++;
+            }
+            br.close();    
+        }catch(IOException e){
+            System.out.println("File not found");
+        }
+        return total;
+        
+    }
+    
+    
+    public String randomTString(){
+        try{
+            int randInt = random.nextInt(totallines());
+            return Files.readAllLines(Paths.get(file.getName())).get(randInt);
+        }catch(IOException e){
+            System.out.println("File not found");
+        }
+        return "";
+    }
+
+}
